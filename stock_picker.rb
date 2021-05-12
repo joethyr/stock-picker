f93cee034caea2083cc8f96b2016fc4ded824916
Joseph Thayer <joethayr@gmail.com>
2021-05-11 19:59:31 -0600

    deleted commented code.

--- stock_picker.rb
+++ stock_picker.rb
@@ -9,9 +9,6 @@ def stockpicker(prices)
   end
   max_profit = profit_days.max_by { |k,v| v }
   p " The best day to buy is on day #{max_profit[0][0]} and sell on day #{max_profit[0][1]} for a maximum profit of $#{max_profit[1]}!"
-
-  # puts my_hash.key(my_hash.values.max)
-  # profit_days.each_index { |e| puts (profit_days[e][1])}
 end
 
 prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
