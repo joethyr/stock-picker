657ef103c0a8401b1ec08ac8f7784a56e10f3f4c
Joseph Thayer <joethayr@gmail.com>
2021-05-11 19:55:54 -0600

    modified code. Using hashes to store values over arrays for ease of accesibility.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,23 +1,19 @@
-prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
-
 def stockpicker(prices)
-  best_p = 0
-  check_p = 0
-  profit = 0
-  prices.each do |e|
-    prices.each do |i|
-      if prices.index(e) < prices.index(i)
-        check_p = i - e
+  profit_days = {}
+  prices.each_with_index do |buyprice, buyday|
+    prices.each_with_index do |sellprice, sellday|
+      if sellday > buyday
+        my_hash[[buyday, sellday]] = sellprice - buyprice
       end
-      if check_p > profit
-        profit = check_p
-      end
-    if profit > best_p
-      best_p = profit
-    end
     end
   end
-  puts best_p
+  max_profit = profit_days.max_by { |k,v| v }
+  p " The best day to buy is on day #{max_profit[0][0]} and sell on day #{max_profit[0][1]} for a maximum profit of $#{max_profit[1]}!"
+
+  # puts my_hash.key(my_hash.values.max)
+  # profit_days.each_index { |e| puts (profit_days[e][1])}
 end
+
+prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
 stockpicker(prices)
